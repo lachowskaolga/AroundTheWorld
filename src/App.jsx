34f8ef625346788b7cb4aee30cbd17e8bb626861
Olga <olga@MacBook-Air-Olga.local>
2025-10-04 17:27:55 +0200

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
+function App(){
+  return <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='product' element={<Product/>}></Route>
+        <Route path='pricing' element={<Pricing/>}></Route>
+        <Route path='*' element={<PageNotFound/>}></Route>
+        <Route path="app" element={<AppLayout/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  
+}
+
+export default App;
